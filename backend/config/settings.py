c5fe468373284fa2de37279eff8c8efd30615e2e
Kevin-Kiprotich <kevinkiprotich0089@gmail.com>
2026-01-18 14:35:36 +0300

    secrets hidden

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -158,11 +158,11 @@ REST_FRAMEWORK = {
 
 # Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kipchirchirkevin012@gmail.com'
-EMAIL_HOST_PASSWORD ='gtynsckxohwpuupi'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-DEFAULT_FROM_EMAIL = 'noreply<admin@PIMS.co.ke>'
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 EXPIRE_AFTER = "1h"
--- backend/forms/urls.py
+++ backend/forms/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BulkSubmissionView
+from .views.submissions import BulkSubmissionView
 
 urlpatterns = [
     path('submissions/bulk/', BulkSubmissionView.as_view(), name='bulk-submission'),
